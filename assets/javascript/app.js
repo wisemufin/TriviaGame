c90e57e6c61b993b6beeb5696690e19ad61bf297
Matthew Trabold <mct4240@gmail.com>
2019-03-11 21:21:30 -0500

    Game finished, need to create a button to reset the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,8 +18,20 @@ var quizQuestions = [
       d: '1999'
     },
     correctAnswer: 'c'
+  },
+  {
+    question: "What year did GoldenEye 007 get released?",
+    answers: {
+      a: '1995',
+      b: '1994',
+      c: '1997',
+      d: '1999'
+    },
+    correctAnswer: 'c'
   }
 ];
+
+
 
 // Keeps track of right answers
 var scoreCorrect = 0;
@@ -29,23 +41,46 @@ var scoreWrong = 0;
 var unanswered = 0;
 // Stores what quesiton the game is currently on
 var questionIndex = 0;
+// Timing variable
+var time = 30;
+// Holds the interval
+var interval;
+
+function endgame() {
+  $("#questions").text("Game Over!");
+  $(".answer").css({
+    'visibility' : 'visible'
+  });
+  $("#answer1").text("Correct: " + scoreCorrect);
+  $("#answer2").text("Wrong: " + scoreWrong);
+  $("#answer3").text("Unanswered: " + unanswered);
+  $("#answer4").text("");
+}
 
 function loadQuestions() {
-  if (questionIndex < (quizQuestions.length)) {
+  if (questionIndex < quizQuestions.length) {
+    time = 30;
+    timerStart();
+    $("#timer").text(time);
     $("#questions").text(quizQuestions[questionIndex].question);
+    $(".answer").css({
+      'visibility' : 'visible'
+    });
     $("#answer1").text(quizQuestions[questionIndex].answers.a);
     $("#answer2").text(quizQuestions[questionIndex].answers.b);
     $("#answer3").text(quizQuestions[questionIndex].answers.c);
     $("#answer4").text(quizQuestions[questionIndex].answers.d);
   } else {
+    // endgame();
     $("#questions").text("Game Over!");
+    $(".answer").css({
+      'visibility' : 'visible'
+    });
     $("#answer1").text("Wins: " + scoreCorrect);
     $("#answer2").text("Losses: " + scoreWrong);
+    $("#answer3").text("Unanswered: " + unanswered);
+    $("#answer4").text("");
   }
-}
-
-function checkAnswer() {
-  
 }
 
 // $("#questions").text(quizQuestions[0].question);
@@ -61,9 +96,72 @@ $("#start").on("click", function() {
   });
 
   loadQuestions();
-
+  timerStart();
 });
 
+// Timer start function
+function timerStart() {
+  clearInterval(interval);
+  interval = setInterval(decrement, 1000);
+  console.log(interval);
+}
+
+// Timer countdown function
+function decrement() {
+  time--;
+  $("#timer").text(time);
+
+  if (time === 0) {
+    stop();
+    unanswered++;
+  }
+}
+
+// Function to stop the timer
+function stop() {
+  clearInterval(interval);
+  questionIndex++;
+  $("#questions").text("Time is up!");
+  $(".answer").css({
+    'visibility' : 'hidden'
+  });
+  setTimeout(loadQuestions, 1000 * 5);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  setTimeout(loadQuestions, 1000 * 5);
+}
+
+// On click function for each quiz question
+$(".answer").on("click",function() {
+  if ($(this).attr("value") === quizQuestions[questionIndex].correctAnswer) {
+    // console.log($(this).attr("value"));
+    resetTimer();
+    $("#questions").text("Correct!");
+    $(".answer").css({
+      'visibility' : 'hidden'
+    });
+    // Increase question index
+    questionIndex++;
+    scoreCorrect++;
+    setTimeout(loadQuestions, 1000 * 5);
+    console.log(questionIndex);
+} else {
+  resetTimer();
+  $("#questions").text("Wrong!");
+  // Hide answers text
+  $(".answer").css({
+    'visibility' : 'hidden'
+  });
+  // Increase wrong score
+  scoreWrong++;
+  // Increase question index
+  questionIndex++;
+  setTimeout(loadQuestions, 1000 * 5);
+  console.log("Wrong!");
+}
+});
 
 // for (let i = 0; i < quizQuestions.length; i++) {
 //   console.log(quizQuestions[i].question);
